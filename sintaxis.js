cd87cef7aeaade1a544ef9d2b316fc513a1d9d10
sergiob1104 <sergiob_1104@hotmail.com>
2023-04-14 09:36:04 -0500

    clase de js

---
+++ sintaxis.js
@@ -0,0 +1,12 @@
+//const constante = "Ejemplo Constante"
+//let variable1 = 1;
+//var variable2 = 2;
+
+//let variabletest1 = "Esto es una prueba"
+//console.log(variabletest1)
+//console.log(constante);
+
+OPERADORES
+
+
+
